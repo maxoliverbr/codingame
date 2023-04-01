c5605852f95ecd4e11fc63b132a8b45c049c9ca6
maxoliverbr <110696335+maxoliverbr@users.noreply.github.com>
2023-04-01 08:25:09 -0300

    Update clashes.py

--- clashes.py
+++ clashes.py
@@ -74,3 +74,18 @@ for c in v:
   r+=c
 print(r)
 """
+
+"""
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+line = input().split()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+for l in line:
+ print(sum(map(ord,l)))
+"""
