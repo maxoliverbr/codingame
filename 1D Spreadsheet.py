1ed86dc0db479c9768c947b5130f12a2c8bc5539
maxoliverbr <110696335+maxoliverbr@users.noreply.github.com>
2023-05-31 06:51:49 -0300

    Update and rename 1D Spreadsheet to 1D Spreadsheet.py
    Accounting is not easy...

--- 1D Spreadsheet.py
+++ 1D Spreadsheet.py
@@ -9,13 +9,13 @@ v = []
 def ADD(x,y,i):
     r = int(x)+int(y)
     v[i]=(str(r),"c")
-    print("Av = ", v, file=sys.stderr, flush=True)
+    print("Av = ", v[i], x,y, file=sys.stderr, flush=True)
     return(r)
 
 def SUB(x,y,i):
     r = int(x)-int(y)
     v[i]=(str(r),"c")
-    print("Sv = ", v, file=sys.stderr, flush=True)
+    print("Sv = ", v[i], file=sys.stderr, flush=True)
     return(r)
 
 def MULT(x,y,i):
@@ -25,7 +25,7 @@ def MULT(x,y,i):
     return(r)
 
 def VALUE(x,y,i):
-    #print("Vv = ", v, file=sys.stderr, flush=True)
+    print("Vv = ", v, file=sys.stderr, flush=True)
     v[i]=(str(x),"0")
     return x
 
@@ -40,28 +40,41 @@ for i in range(n):
     o.append((operation,arg_1,arg_2))
 
 #print(n, file=sys.stderr, flush=True)
-print("o = ", o, file=sys.stderr, flush=True)
+#print("o = ", o, file=sys.stderr, flush=True)
 
-
+print("-----", file=sys.stderr, flush=True)   
 for i in range(n):
+    print("Peval= ",f"{o[i][0]}({o[i][1]},{o[i][2]},{i})", file=sys.stderr, flush=True)    
     if o[i][0]=="VALUE":
         v.append((o[i][1],o[i][2]))
     else:
         v.append(('0','nc'))
-print("v = ", v, file=sys.stderr, flush=True)
+    if "$" not in o[i][1] and "$" not in o[i][2]:
+        
+        f =    o[i][0]
+        arg1 = int(o[i][1].replace("$",""))
+        arg2 = int(o[i][2].replace("$",""))
+        
+        print("eval=  ",f"{f}({arg1},{arg2},{i})", file=sys.stderr, flush=True)    
+        r = eval(f"{f}({arg1},{arg2},{i})")
+
+#print("v = ", v, file=sys.stderr, flush=True)
 
 oo=[]
 calc_start = False
 calc_end = False
 r=0
+print("-----", file=sys.stderr, flush=True)   
 while calc_end == False:
     for i in range(n):
         
         f =    o[i][0]
         arg1 = o[i][1]
         arg2 = o[i][2]
-        print(i,f,arg1,arg2, file=sys.stderr, flush=True)
-
+        #print(i,f,arg1,arg2, file=sys.stderr, flush=True)
+        #print("eval= ",f"{o[i][0]}({arg1},{arg2},{i})", file=sys.stderr, flush=True)    
+        
+        print("X-",f"{o[i][0]}({arg1},{arg2},{i})", file=sys.stderr, flush=True)        
         if f != "VALUE":
             if "$" in arg1 and "$" in arg2 and calc_start == False:
                 oo.append(None)
@@ -73,15 +86,16 @@ while calc_end == False:
                 else:
                     arg1 = int(arg1)
                 
-                if "$" in arg2:
+                if "$" in arg2:                
                     arg2 = arg2.replace("$","")
                     arg2 = v[int(arg2)][0]
                 else:
                     arg2 = int(arg2)
+                
+                #print(f"{o[i][0]}({arg1},{arg2},{i})", file=sys.stderr, flush=True)        
                 oo.append(eval(f"{o[i][0]}({arg1},{arg2},{i})"))
         else:
             calc_start = True
-            #oo.append(int(arg1))
             oo.append(eval(f"{o[i][0]}({arg1},{arg2},{i})"))
 
             #print("ev ", i,f,arg1,arg2, file=sys.stderr, flush=True)
