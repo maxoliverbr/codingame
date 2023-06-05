0cf83717b0f6bca508e87031d7203f535fc53aad
maxoliverbr <110696335+maxoliverbr@users.noreply.github.com>
2023-06-05 12:26:57 -0300

    Update Shadows of the Knight - Episode 1.py
    progress....

--- Shadows of the Knight - Episode 1.py
+++ Shadows of the Knight - Episode 1.py
@@ -29,6 +29,7 @@ d = {
 
 dx0=dy0=0
 i = 1
+p_dir = ""
 # game loop
 while True:
     bomb_dir = input()  # the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
@@ -37,11 +38,25 @@ while True:
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
+    if p_dir != bomb_dir:
+        if   (p_dir == "U" and bomb_dir == "D")  or (p_dir =="D" and bomb_dir=="U"):
+            pass
+        elif (p_dir == "U" and bomb_dir == "DR") or (p_dir =="D" and bomb_dir=="UR"):
+            pass
+        elif (p_dir == "U" and bomb_dir == "DR") or (p_dir =="D" and bomb_dir=="UR"):
+            pass
+        elif (p_dir == "U" and bomb_dir == "DR") or (p_dir =="D" and bomb_dir=="UR"):
+            pass
+        
+        i+=1
+    else:
+        i=2
+
     dx0 = (d[bomb_dir][0])/i
     dy0 = (d[bomb_dir][1])/i
 
-    x0 += round(dx0)
-    y0 += round(dy0)
+    x0 += math.floor(dx0)
+    y0 += math.floor(dy0)
 
     if x0>=w:
         x0=w-1
@@ -53,7 +68,11 @@ while True:
     elif y0<0:
         y0=0
     
+    p_dir = bomb_dir
+    p_x   = x0
+    p_y   = y0
+
     print("DD = ", dx0, dy0, file=sys.stderr, flush=True)        
     # the location of the next window Batman should jump to.
     print(f"{x0} {y0}")
-    i+=2
+    #i+=1
