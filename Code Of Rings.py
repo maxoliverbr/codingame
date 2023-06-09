a264d069ff533e4f414def072e40d461ddae6056
maxoliverbr <110696335+maxoliverbr@users.noreply.github.com>
2023-06-09 17:36:55 -0300

    Update Code Of Rings.py

--- Code Of Rings.py
+++ Code Of Rings.py
@@ -19,16 +19,19 @@ ilb=mc=m=0
 d="+"
 for l in range(len(magic_phrase)):
     nc = ""
-    lb = buffer[l%30]
-    #if lb == "A":
-    #    buffer = buffer[:(l%30)] + magic_phrase[l] + buffer[l%30+1:]
-    #    mc = ord(magic_phrase[l])-64
-    #    m = mc
-    #else:
     c_buffer = buffer[l%30]
-    ord_c_buffer = ord(c_buffer)
+    
+    if c_buffer == "@":
+        ord_c_buffer = 64
+    else:    
+        ord_c_buffer = ord(c_buffer)
+    
     ord_c_message = ord(magic_phrase[l])
-    if ord_c_buffer>ord_c_message:
+
+    if c_buffer == "@" and magic_phrase[l] == " ":
+        d=""
+        d_ord=0
+    elif ord_c_buffer>ord_c_message:
         d="-"
         d_ord = ord_c_buffer - ord_c_message
     else:
@@ -37,8 +40,8 @@ for l in range(len(magic_phrase)):
     buffer = buffer[:l%30] + magic_phrase[l] + buffer[l%30+1:]
     m = d_ord
     #print(f"{l}, {l%30} '{magic_phrase[l]}' '{buffer[l%30]}' {ord_c_buffer} {ord_c_message} m={m} {d} {c_buffer}", file=sys.stderr, flush=True)   
-    lb = buffer[l%30]
-    #print(f"{l}, {l%30} '{magic_phrase[l]}' '{buffer[l%30]}' ilb={ilb} m={m} mc={mc} {ilb} {d}", file=sys.stderr, flush=True)
+    
+    print(f"{l}, {l%30} '{magic_phrase[l]}' '{buffer[l%30]}' m={m} {d}", file=sys.stderr, flush=True)
     o += d*m+".>"
 print(len(o), file=sys.stderr, flush=True)
 print(buffer, file=sys.stderr, flush=True)
