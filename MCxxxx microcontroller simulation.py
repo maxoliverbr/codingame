e2da7e4bb8933f796ffb4d3ab97fba1380f9b59a
Max Oliver <110696335+maxoliverbr@users.noreply.github.com>
2023-06-16 16:46:02 -0300

    Create MCxxxx microcontroller simulation.py

---
+++ MCxxxx microcontroller simulation.py
@@ -0,0 +1,56 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+class CPU:
+    dat = 0
+    acc = 0
+    x0  = 0
+    x1  = 0
+
+
+def MOV(x,y):
+    pass
+
+def ADD(x):
+    pass
+
+def SUB(x):
+    pass
+
+def MUL(x):
+    pass
+
+def NOT():
+    pass
+
+def DGT(x):
+    pass
+
+def DST(x):
+    pass
+
+def TEQ(x,y):
+    pass
+
+def TGT(x,y):
+    pass
+
+def TLT(x,y):
+    pass
+
+def TCP(x,y):
+    pass
+
+k = int(input())
+for i in input().split():
+    input_data = int(i)
+n = int(input())
+for i in range(n):
+    line_of_code = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("output data")
