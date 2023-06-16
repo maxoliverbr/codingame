e1dd1b46d3d4622f7ac4d09839465337eae8a0ce
Max Oliver <110696335+maxoliverbr@users.noreply.github.com>
2023-06-15 22:59:26 -0300

    Update Micro Assembly.py
    100%

--- Micro Assembly.py
+++ Micro Assembly.py
@@ -8,16 +8,14 @@ class Registers:
     b = 0
     c = 0 
     d = 0
+    line = 0
 
 register = Registers()
 instructions = []
 
 register.a, register.b, register.c, register.d = [int(i) for i in input().split()]
-print(register.a,register.b,register.c,register.d, file=sys.stderr, flush=True)
 
 def MOV(r,val):
-    print(f"{r}, val ",r, val, file=sys.stderr, flush=True)
-    
     if val.isalpha():val = f"register.{val}"
     if   r == "a":register.a = eval(val)
     elif r == "b":register.b = eval(val)
@@ -25,18 +23,18 @@ def MOV(r,val):
     elif r == "d":register.d = eval(val)
     
 
-def ADD(r, val1, val2):
-    print(f"{r}, val1, val2 ",r, val1,val2, file=sys.stderr, flush=True)
+def ADD(r, val1, val2):    
     if val1.isalpha():val1 = f"register.{val1}"
     if val2.isalpha():val2 = f"register.{val2}"
     if   r == "a":register.a = eval(val1)+eval(val2)
     elif r == "b":register.b = eval(val1)+eval(val2)
     elif r == "c":register.c = eval(val1)+eval(val2)
     elif r == "d":register.d = eval(val1)+eval(val2)
+
     
 
 def SUB(r,val1,val2):
-    print(f"{r}, val1, val2 ",r, val1,val2, file=sys.stderr, flush=True)
+    print(f"SUB {r}, {val1}, {val2} ", file=sys.stderr, flush=True)
     if val1.isalpha():val1 = f"register.{val1}"
     if val2.isalpha():val2 = f"register.{val2}"
     if   r == "a":register.a = eval(val1)-eval(val2)
@@ -46,15 +44,15 @@ def SUB(r,val1,val2):
     
 
 def JNE(imm,val1,val2):
-    print(f"{imm}, {val1}, {val2} ", file=sys.stderr, flush=True)
+    print(f"JNE {imm}, {val1}, {val2}", file=sys.stderr, flush=True)
     if val1.isalpha():val1 = f"register.{val1}"
     if val2.isalpha():val2 = f"register.{val2}"
-    
+    res_1 = eval(val1)
+    res_2 = eval(val2)
    
-    if val1 != val2:
-        return eval(imm)
+    if res_1 != res_2:
+        register.line = int(imm)-1
     
-
 
 #a, b, c, d = [int(i) for i in input().split()]
 n = int(input())
@@ -62,24 +60,23 @@ n = int(input())
 for i in range(n):
     instructions.append(input())
 
-for instruction in instructions:
-    print(register.a,register.b,register.c,register.d, file=sys.stderr, flush=True)
-    i = instruction.split()
+register.line = 0
+
+while True:
+    
+    i = instructions[register.line].split()
 
     if i[0]=="MOV":
-        print((f"func = {i[0]}({i[1]},{i[2]})"), file=sys.stderr, flush=True)
         r = eval(f"{i[0]}('{i[1]}','{i[2]}')")
     elif i[0]=="ADD" or i[0]=="SUB":
-        print((f"func = {i[0]}({i[1]},{i[2]},{i[3]})"), file=sys.stderr, flush=True)
         r = eval(f"{i[0]}('{i[1]}','{i[2]}','{i[3]}')")
     elif i[0] == "JNE":
-        print((f"func = {i[0]}({i[1]},{i[2]},{i[3]})"), file=sys.stderr, flush=True)
         r = eval(f"{i[0]}('{i[1]}','{i[2]}','{i[3]}')")
-
-print(register.a,register.b,register.c,register.d)
-
-
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
+    
+    register.line+=1
+    
+    if register.line>=n:
+        print(register.a,register.b,register.c,register.d)
+        sys.exit()
+    
 
